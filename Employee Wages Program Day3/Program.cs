b1864b5475150ee2441d600f16720da7ecb26efa
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-18 14:59:16 +0530

    Modified to PartTime Employee Wages program

--- Employee Wages Program Day3/Program.cs
+++ Employee Wages Program Day3/Program.cs
@@ -8,21 +8,32 @@ namespace Employee_Wages_Program_Day3
         {
             Console.WriteLine("Welcome to employee wages computation program!");
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            int IS_PRESENT = 1;
-            int EMP_RATE_PER_HOUR = 20;
+            int empCheck = random.Next(0, 3);
+
+            int IS_FULLTIME = 1;
+            int IS_PARTTIME = 2;
+            int EMP_RATE_PER_HRS = 20;
+
             int empHrs = 0;
             int empWages = 0;
-            if (empCheck == IS_PRESENT)
+
+            if (empCheck == IS_FULLTIME)
             {
                 empHrs = 8;
+                Console.WriteLine("Employee is present and doing Fulltime");
+            }
+            else if (empCheck == IS_PARTTIME)
+            {
+                empHrs = 4;
+                Console.WriteLine("Employee is present and doing Parttime");
             }
             else
             {
                 empHrs = 0;
+                Console.WriteLine("Employee is absent");
             }
-            empWages = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee wages: " + empWages);
+            empWages = empHrs * EMP_RATE_PER_HRS;
+            Console.WriteLine("Employee wages:" + empWages);
         }
     }
 }
