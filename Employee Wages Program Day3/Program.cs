8746687b2c19840912158d01287566ee9cf25f98
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-18 15:56:09 +0530

    Modified to Monthly Wages of Employee

--- Employee Wages Program Day3/Program.cs
+++ Employee Wages Program Day3/Program.cs
@@ -7,26 +7,32 @@ namespace Employee_Wages_Program_Day3
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to employee wages computation program!");
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            int empHrs = 0, empWages = 0, EMP_RATE_PER_HRS = 20;
-            switch(empCheck)
-            { 
-                case 1:
-                    empHrs = 8;
-                    Console.WriteLine("Employee is present and doing Fulltime");
-                    break;
-                case 2:
-                    empHrs = 4;
-                    Console.WriteLine("Employee is present and doing Parttime");
-                    break;
-                default:
-                    empHrs = 0;
-                    Console.WriteLine("Employee is absent");
-                    break;
+            int empHrs = 0;
+            int empWages = 0;
+            int totalWages = 0;
+            int NUM_OF_WORKING_DAYS = 20, EMP_RATE_PER_HRS = 20;
+
+            for ( int Day = 1; Day <= NUM_OF_WORKING_DAYS; Day++)
+            {
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case 1:
+                        empHrs = 4;
+                        break;
+                    case 2:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+                empWages = empHrs * EMP_RATE_PER_HRS;
+                totalWages = totalWages + empWages;
             }
-            empWages = empHrs * EMP_RATE_PER_HRS;
-            Console.WriteLine("Employee wages:" + empWages);
+            Console.WriteLine("Total monthly wages of an employee is: " + totalWages);
         }
     }
 }
