f0a8cb0cb52f26ae5fb457b3b4ca8feb57ca9e49
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-18 15:32:24 +0530

    Modified Employee Wages using Switch Case

--- Employee Wages Program Day3/Program.cs
+++ Employee Wages Program Day3/Program.cs
@@ -9,28 +9,21 @@ namespace Employee_Wages_Program_Day3
             Console.WriteLine("Welcome to employee wages computation program!");
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-
-            int IS_FULLTIME = 1;
-            int IS_PARTTIME = 2;
-            int EMP_RATE_PER_HRS = 20;
-
-            int empHrs = 0;
-            int empWages = 0;
-
-            if (empCheck == IS_FULLTIME)
-            {
-                empHrs = 8;
-                Console.WriteLine("Employee is present and doing Fulltime");
-            }
-            else if (empCheck == IS_PARTTIME)
-            {
-                empHrs = 4;
-                Console.WriteLine("Employee is present and doing Parttime");
-            }
-            else
-            {
-                empHrs = 0;
-                Console.WriteLine("Employee is absent");
+            int empHrs = 0, empWages = 0, EMP_RATE_PER_HRS = 20;
+            switch(empCheck)
+            { 
+                case 1:
+                    empHrs = 8;
+                    Console.WriteLine("Employee is present and doing Fulltime");
+                    break;
+                case 2:
+                    empHrs = 4;
+                    Console.WriteLine("Employee is present and doing Parttime");
+                    break;
+                default:
+                    empHrs = 0;
+                    Console.WriteLine("Employee is absent");
+                    break;
             }
             empWages = empHrs * EMP_RATE_PER_HRS;
             Console.WriteLine("Employee wages:" + empWages);
