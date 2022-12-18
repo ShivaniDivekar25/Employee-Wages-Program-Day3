4a58763572e0a55e31530a49a2c612dbe132c516
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-18 14:33:26 +0530

    Modified Employee is present or absent by using RANDOM

--- Employee Wages Program Day3/Program.cs
+++ Employee Wages Program Day3/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Employee_Wages_Program_Day3
+﻿using System;
+
+namespace Employee_Wages_Program_Day3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to employee wages computation program!");
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            int IS_PRESENT = 1;
+            if (empCheck == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }
