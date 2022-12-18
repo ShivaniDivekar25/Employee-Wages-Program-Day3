cc66dbb77182d03481df263fecb6938673ddf65d
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-18 14:19:52 +0530

    Added Welcome Employee Wages Program

---
+++ Employee Wages Program Day3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Employee_Wages_Program_Day3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wages computation program!");
+        }
+    }
+}
