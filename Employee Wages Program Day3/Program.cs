a64363c9f7015f2bc683b13e313cb281f3eebbca
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-18 14:47:05 +0530

    Modified Daily Employee Wage program

--- Employee Wages Program Day3/Program.cs
+++ Employee Wages Program Day3/Program.cs
@@ -10,14 +10,19 @@ namespace Employee_Wages_Program_Day3
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             int IS_PRESENT = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWages = 0;
             if (empCheck == IS_PRESENT)
             {
-                Console.WriteLine("Employee is present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is absent");
+                empHrs = 0;
             }
+            empWages = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee wages: " + empWages);
         }
     }
 }
