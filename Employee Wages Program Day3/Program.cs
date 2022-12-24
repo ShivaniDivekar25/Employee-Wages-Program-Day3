ac8b9ba3ab5e71714613a66d628c95a5b2c676d7
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-24 19:48:41 +0530

    Modified Computation Of Employee Wages using Class Method

--- Employee Wages Program Day3/Program.cs
+++ Employee Wages Program Day3/Program.cs
@@ -4,22 +4,33 @@ namespace Employee_Wages_Program_Day3
 {
     internal class Program
     {
+        public const int IS_PARTTIME = 1;
+        public const int IS_FULLTIME = 2;
+        public const int EMP_RATE_PER_HRS = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_RATE_IN_MONTH = 100;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to employee wages computation program!");
-            int empHrs = 0, totalEmpHrs=0, totalWorkingDays = 0; 
-            int NUM_OF_WORKING_DAYS = 20, EMP_RATE_PER_HRS = 20, MAX_RATE_IN_MONTH = 100; 
+            CalculateWages();
+            Console.ReadLine();
+        }
+
+        public static void CalculateWages()
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            Random random5 = new Random();
+
             while (totalEmpHrs < MAX_RATE_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
                 totalWorkingDays++;
+                int empCheck = random5.Next(0, 3);
                 switch (empCheck)
                 {
-                    case 1:
+                    case IS_PARTTIME:
                         empHrs = 4;
                         break;
-                    case 2:
+                    case IS_FULLTIME:
                         empHrs = 8;
                         break;
                     default:
