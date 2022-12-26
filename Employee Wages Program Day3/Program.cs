b5d5b3f2a17ea1018ab8536a7f24b3e7c0e0f972
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-27 00:46:09 +0530

    Modified Employee Wages for Multiple Company

--- Employee Wages Program Day3/Program.cs
+++ Employee Wages Program Day3/Program.cs
@@ -2,26 +2,26 @@
 
 namespace Employee_Wages_Program_Day3
 {
-    internal class Program
+    internal class Program      //Created class
     {
         public const int IS_PARTTIME = 1;
         public const int IS_FULLTIME = 2;
-        public const int EMP_RATE_PER_HRS = 20;
-        public const int NUM_OF_WORKING_DAYS = 20;
-        public const int MAX_RATE_IN_MONTH = 100;
-        static void Main(string[] args)
+       
+        static void Main(string[] args)     //Calling methods in main method
         {
             Console.WriteLine("Welcome to employee wages computation program!");
-            CalculateWages();
+            CalculateWages("Bridgelabz",30,25,70);
+            CalculateWages("Deloitte",40,35,80);
+            CalculateWages("Microsoft", 35, 45, 70);
             Console.ReadLine();
         }
 
-        public static void CalculateWages()
+        public static void CalculateWages(string company, int empRateHrs, int maxWorkingDays,int maxWorkingHrs)         //Creating method for wages
         {
-            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-            Random random5 = new Random();
+            int empHrs = 0, empWage = 0, totalEmpHrs = 0, totalWorkingDays = 0, totalEmpWages = 0;          //Initializing value
+            Random random5 = new Random();                                                                          
 
-            while (totalEmpHrs < MAX_RATE_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            while (totalEmpHrs < maxWorkingHrs && totalWorkingDays < maxWorkingDays)            //While loop for required condition and computation
             {
                 totalWorkingDays++;
                 int empCheck = random5.Next(0, 3);
@@ -37,12 +37,12 @@ namespace Employee_Wages_Program_Day3
                         empHrs = 0;
                         break;
                 }
-
+                empWage = empRateHrs * empHrs;
                 totalEmpHrs = totalEmpHrs + empHrs;
-                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs: " + empHrs);
+                totalEmpWages = totalEmpHrs * empRateHrs;
+               
             }
-            int totalEmpWages = totalEmpHrs * EMP_RATE_PER_HRS;
-            Console.WriteLine("Total Employee wages:" + totalEmpWages);
+            Console.WriteLine("Total Employee wages for {0} {1} days:{2} and Hrs:{3}", company,(totalWorkingDays-1),totalEmpWages,(totalEmpHrs-empHrs));
         }
     }
 }
