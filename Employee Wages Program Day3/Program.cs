65325c50abe499706af9c29ffccbfdd13b75f7f1
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-28 13:08:17 +0530

    Modified to Total Wages Computation for Each Company program

--- Employee Wages Program Day3/Program.cs
+++ Employee Wages Program Day3/Program.cs
@@ -2,21 +2,33 @@
 
 namespace Employee_Wages_Program_Day3
 {
-    internal class Program      //Created class
+    class CompanyWage     //Created class
     {
         public const int IS_PARTTIME = 1;
         public const int IS_FULLTIME = 2;
-       
+        public string company;
+        public int empRatePerHrs, maxWorkingDays, maxWorkingHrs;
+
+        public CompanyWage(string company, int empRatePerHrs, int maxWorkingDays, int maxWorkingHrs)
+        {
+            this.company = company;
+            this.empRatePerHrs = empRatePerHrs;
+            this.maxWorkingDays = maxWorkingDays;
+            this.maxWorkingHrs = maxWorkingHrs;
+        }
+
         static void Main(string[] args)     //Calling methods in main method
         {
             Console.WriteLine("Welcome to employee wages computation program!");
-            CalculateWages("Bridgelabz",30,25,70);
-            CalculateWages("Deloitte",40,35,80);
-            CalculateWages("Microsoft", 35, 45, 70);
-            Console.ReadLine();
+            CompanyWage BridgeLabz = new CompanyWage("Bridgelabz", 30, 25, 70);
+            BridgeLabz.CalculateWages();
+            CompanyWage Delloite = new CompanyWage("Deloitte", 40, 35, 80);
+            Delloite.CalculateWages();
+            CompanyWage Microsoft = new CompanyWage("Microsoft", 35, 45, 70);
+            Microsoft.CalculateWages();
         }
 
-        public static void CalculateWages(string company, int empRateHrs, int maxWorkingDays,int maxWorkingHrs)         //Creating method for wages
+        public void CalculateWages()         //Creating method for wages
         {
             int empHrs = 0, empWage = 0, totalEmpHrs = 0, totalWorkingDays = 0, totalEmpWages = 0;          //Initializing value
             Random random5 = new Random();                                                                          
@@ -37,9 +49,9 @@ namespace Employee_Wages_Program_Day3
                         empHrs = 0;
                         break;
                 }
-                empWage = empRateHrs * empHrs;
+                empWage = empRatePerHrs * empHrs;
                 totalEmpHrs = totalEmpHrs + empHrs;
-                totalEmpWages = totalEmpHrs * empRateHrs;
+                totalEmpWages = totalEmpHrs * empRatePerHrs;
                
             }
             Console.WriteLine("Total Employee wages for {0} {1} days:{2} and Hrs:{3}", company,(totalWorkingDays-1),totalEmpWages,(totalEmpHrs-empHrs));
