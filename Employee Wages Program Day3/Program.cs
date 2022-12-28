ab3ffbd11cda38be060e6e9e516bf367e3ef40b1
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-28 15:21:22 +0530

    Modified to Manage Employee Wages for Multiple Comapny program

--- Employee Wages Program Day3/Program.cs
+++ Employee Wages Program Day3/Program.cs
@@ -2,38 +2,56 @@
 
 namespace Employee_Wages_Program_Day3
 {
-    class CompanyWage     //Created class
+    class CompanyWageBuilder     //Created class
     {
         public const int IS_PARTTIME = 1;
         public const int IS_FULLTIME = 2;
-        public string company;
-        public int empRatePerHrs, maxWorkingDays, maxWorkingHrs;
+        CompanyEmpWage[] companies;
+        int numbOfCompanies = 0;
 
-        public CompanyWage(string company, int empRatePerHrs, int maxWorkingDays, int maxWorkingHrs)
+        public CompanyWageBuilder()
         {
-            this.company = company;
-            this.empRatePerHrs = empRatePerHrs;
-            this.maxWorkingDays = maxWorkingDays;
-            this.maxWorkingHrs = maxWorkingHrs;
+            companies = new CompanyEmpWage[5];
         }
 
+        public void AddComapnyDetailsIntoArray(string company, int empRatePerHrs, int maxWorkingDays, int maxWorkingHrs)
+        {
+            CompanyEmpWage comp = new CompanyEmpWage(company, empRatePerHrs, maxWorkingDays, maxWorkingHrs);
+            companies[numbOfCompanies] = comp;
+            numbOfCompanies++;
+        }
+
+        public void IterateOverCompany()
+        {
+            for(int i=0; i< numbOfCompanies; i++)
+            {
+                int totalEmpWage = CalculateWages(companies[i]);
+                companies[i].SetTotalWage(totalEmpWage);
+                Console.WriteLine(companies[i].ToString());
+            }
+        }
         static void Main(string[] args)     //Calling methods in main method
         {
             Console.WriteLine("Welcome to employee wages computation program!");
-            CompanyWage BridgeLabz = new CompanyWage("Bridgelabz", 30, 25, 70);
+            CompanyWageBuilder builder = new CompanyWageBuilder();
+            builder.AddComapnyDetailsIntoArray("Bridgelabz", 30, 25, 70);
+            builder.AddComapnyDetailsIntoArray("Deloitte", 40, 35, 80);
+            builder.AddComapnyDetailsIntoArray("Microsoft", 35, 45, 70);
+            builder.IterateOverCompany();
+           /*CompanyWage BridgeLabz = new CompanyWage("Bridgelabz", 30, 25, 70);
             BridgeLabz.CalculateWages();
             CompanyWage Delloite = new CompanyWage("Deloitte", 40, 35, 80);
             Delloite.CalculateWages();
             CompanyWage Microsoft = new CompanyWage("Microsoft", 35, 45, 70);
-            Microsoft.CalculateWages();
+            Microsoft.CalculateWages();*/
         }
 
-        public void CalculateWages()         //Creating method for wages
+        public int CalculateWages(CompanyEmpWage companyDetails)         //Creating method for wages
         {
             int empHrs = 0, empWage = 0, totalEmpHrs = 0, totalWorkingDays = 0, totalEmpWages = 0;          //Initializing value
             Random random5 = new Random();                                                                          
 
-            while (totalEmpHrs < maxWorkingHrs && totalWorkingDays < maxWorkingDays)            //While loop for required condition and computation
+            while (totalEmpHrs < companyDetails.maxWorkingHrs && totalWorkingDays < companyDetails.maxWorkingDays)            //While loop for required condition and computation
             {
                 totalWorkingDays++;
                 int empCheck = random5.Next(0, 3);
@@ -49,12 +67,13 @@ namespace Employee_Wages_Program_Day3
                         empHrs = 0;
                         break;
                 }
-                empWage = empRatePerHrs * empHrs;
+                empWage = companyDetails.empRatePerHrs * empHrs;
                 totalEmpHrs = totalEmpHrs + empHrs;
-                totalEmpWages = totalEmpHrs * empRatePerHrs;
+                totalEmpWages = totalEmpHrs * companyDetails.empRatePerHrs;
                
             }
-            Console.WriteLine("Total Employee wages for {0} {1} days:{2} and Hrs:{3}", company,(totalWorkingDays-1),totalEmpWages,(totalEmpHrs-empHrs));
+            //Console.WriteLine("Total Employee wages for {0} {1} days:{2} and Hrs:{3}", companyDetails.company, (totalWorkingDays-1),totalEmpWages,(totalEmpHrs-empHrs));
+            return totalEmpWages;
         }
     }
 }
